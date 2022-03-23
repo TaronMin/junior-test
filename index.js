2b726b277fd93f54b88c1dbff8e51c9040dec8ca
TaronMin <taronminasyan@bk.ru>
2022-03-23 18:13:30 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+  let sum = 0;
+  cartItemsPrices.forEach((val)=>{
+      sum+= val * 100;
+  })
+
+  return (sum/100) === WINNING_SUM;
 }
 
 module.exports = doesCartWinPrize;
